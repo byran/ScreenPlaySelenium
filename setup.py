f35a53d0ac49e8563fa0936886aa21e19e855a4d
Byran Wills-Heath <byran@adgico.co.uk>
2021-08-16 21:08:53 +0100

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='screenplay_selenium',
     version='1.0.0',
     description='Selenium support for the Screen play pattern in Python',
-    url='https://github.com/byran/Python-ScreenPlay',
+    url='https://github.com/byran/ScreenPlaySelenium',
     author='Byran Wills-Heath',
     author_email='byran@adgico.co.uk',
     license='MIT',
@@ -15,5 +15,9 @@ setup(
         'screenplay_selenium.matchers',
         'screenplay_selenium.tasks',
     ],
-    zip_safe=False
+    zip_safe=False,
+    install_requires=[
+        'selenium >= 3.141.0',
+        'ScreenPlay@git+git://github.com/byran/ScreenPlay@master#ogg=ScreenPlay'
+    ]
 )
