6fb37de50e5740558c9b23f847d4a385b30dfd13
Byran Wills-Heath <byran@adgico.co.uk>
2022-09-01 11:08:39 +0100

    Add finding elements from stored elements

--- screenplay_selenium/actions/_find_redirect.py
+++ screenplay_selenium/actions/_find_redirect.py
@@ -9,13 +9,20 @@ class find_redirect(find_and_store_as):
     def __init__(self):
         super().__init__()
         self._location: WebElement = None
+        self._state_id: str = None
 
     def in_WebElement(self, location: WebElement):
         self._location = location
         return self
 
+    def in_stored_element(self, id: str):
+        self._state_id = id
+        return self
+
     def _search_location(self, actor: Actor):
         if self._location is not None:
             return self._location
+        elif self._state_id is not None:
+            return actor.state[self._state_id].value
         else:
             return waiting_browser_for(actor, (StaleElementReferenceException, NoSuchElementException))
